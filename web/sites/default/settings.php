926303f77297bcc03d14b328ff895fb05c075952
Nicholas Stees <nstees@gmail.com>
2020-11-04 22:43:57 -0500

    hash to match ddev

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -8,7 +8,7 @@ $settings['update_free_access'] = FALSE;
 $settings['config_sync_directory'] = '../config/default';
 $settings['config_vcs_directory'] = '../config/default';
 
-$settings['hash_salt'] = 'CHANGE_THIS_123';
+$settings['hash_salt'] = 'iJmBXYFnrxvZyDdLgOdbomBpwZUlLjJndqBIhwFCdRmAfBNkLlTHRKSQZaaxoAnl';
 
 // #ddev-generated: Automatically generated Drupal settings file.
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
