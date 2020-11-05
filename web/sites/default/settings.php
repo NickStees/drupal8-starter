6b89620e0ce232b3b814ab9b542101b9b4830f58
Nicholas Stees <nstees@gmail.com>
2020-11-04 21:32:02 -0500

    Add settings php settings to repo

---
+++ web/sites/default/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+$settings['update_free_access'] = FALSE;
+
+/**
+ * Place the config directory outside of the Drupal root.
+ */
+$settings['config_sync_directory'] = '../config/default';
+$settings['config_vcs_directory'] = '../config/default';
+
+// #ddev-generated: Automatically generated Drupal settings file.
+if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
+  include $app_root . '/' . $site_path . '/settings.ddev.php';
+}
